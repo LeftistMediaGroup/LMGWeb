d9af487b3271f7aa9896699fa5cec2a68637675d
DanielATucker <leftistmediagroup@gmail.com>
2022-12-08 10:56:40 -0500

    Added main and home

---
+++ lmgweb/components/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class Home extends React {
+    render() {
+        return (
+            <>
+
+            <h1> Welcome to Leftist Media Group </h1>           
+
+            </>
+        );
+    };
+};
---
+++ lmgweb/components/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Home from "./home";
+
+
+export default class Main extends React {
+    render() {
+        return (
+            <>
+            
+            <Home></Home>
+            
+            </>
+        );
+    };
+};
