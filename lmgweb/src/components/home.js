69af98a89e205694f5ac167695eb848d2a9d86bf
DanielATucker <leftistmediagroup@gmail.com>
2022-12-08 11:36:50 -0500

    Added under construction message

--- lmgweb/src/components/home.js
+++ lmgweb/src/components/home.js
@@ -16,6 +16,15 @@ export default class Home extends React.Component {
 
             <h1> Welcome to Leftist Media Group </h1>           
 
+            <h2> Organize the Left, start the Revolution. </h2>
+            
+            <p>
+                This site is currently under development as a proof of concept. <br></br>
+
+                If you would like to contact the admin, please send an email to LeftistMediaGroup.com<br></br>
+
+            </p>
+
             </>
         );
     };
