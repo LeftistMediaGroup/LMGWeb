70a2c836b769c23820aeb0954059b0f5a34e93e8
DanielATucker <leftistmediagroup@gmail.com>
2022-12-09 13:27:48 -0500

    T

--- lmgweb/src/components/navbar.js
+++ lmgweb/src/components/navbar.js
@@ -1,39 +1,27 @@
 import React from "react";
+import { Text } from "react";
 
 import { AppBar, Toolbar, Typography, Stack, Button } from "@mui/material";
 
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Link } from "react-router-native";
 
-import Education from "./education.js";
 
 
 export const Navbar = () => {
-    const router = createBrowserRouter([
-        {
-          path: "/",
-          element: <Education />,
-        },
-    ]);
-
-    ReactDOM.createRoot(document.getElementById("root")).render(
-        <React.StrictMode>
-          <RouterProvider router={router} />
-        </React.StrictMode>
-      );
-      
     return (
         <AppBar position='static'>
             <Toolbar size = 'large' edge = 'start'>
                 <Typography variant="h6" sx= {{ flexGrow: 1}}>
                     Home
                 </Typography>
+
                 <Stack direction = "row" spacing={2}>
                     <Button color = "inherit">
-                        Education
+                        <Link to="/education">
+                            <Text>
+                                Education
+                            </Text>
+                        </Link>
                     </Button>
                 </Stack>
             </Toolbar>
