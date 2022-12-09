bfc13a0124419b63527bcbaef5870c17cc6b9ba8
DanielATucker <leftistmediagroup@gmail.com>
2022-12-09 12:07:08 -0500

    Added Cards

--- lmgweb/src/components/home.js
+++ lmgweb/src/components/home.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Card from '@mui/material/Card';
+
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -14,17 +16,21 @@ export default class Home extends React.Component {
         return (
             <>
 
-            <h1> Welcome to Leftist Media Group </h1>           
+            <Card>
+                <h1> Welcome to Leftist Media Group </h1>           
 
-            <h2> Organize the Left, start the Revolution. </h2>
+                <h2> Organize the Left, start the Revolution. </h2>
+            </Card>
+
+            <Card>
+                <p>
+                    This site is currently under development as a proof of concept. <br></br>
+
+                    If you would like to contact the admin, please send an email to LeftistMediaGroup.com<br></br>
+
+                </p>
+            </Card>
             
-            <p>
-                This site is currently under development as a proof of concept. <br></br>
-
-                If you would like to contact the admin, please send an email to LeftistMediaGroup.com<br></br>
-
-            </p>
-
             </>
         );
     };
