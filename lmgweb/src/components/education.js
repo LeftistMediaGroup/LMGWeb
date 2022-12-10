15ae24e7cf8374d7172327eada06fbab1cd02f60
DanielATucker <leftistmediagroup@gmail.com>
2022-12-09 21:26:55 -0500

    T

--- lmgweb/src/components/education.js
+++ lmgweb/src/components/education.js
@@ -15,29 +15,32 @@ export default class Education extends React.Component {
     render() {
         return (
             <>
-            
-            <Card variant = "outlined" sx = {{p: 1, m: 1}}>
-
-                <h1> Education </h1>           
-            </Card>
 
             <Card variant = "outlined" sx = {{p: 1, m: 1}}>
 
-                <p>
-                    This page is soon to be filled with lots of Educational leftist content.
-                </p>
+                <Card variant = "outlined" sx = {{p: 1, m: 1}}>
+
+                    <h1> Education </h1>           
+                </Card>
+
+                <Card variant = "outlined" sx = {{p: 1, m: 1}}>
+
+                    <p>
+                        This page is soon to be filled with lots of Educational leftist content.
+                    </p>
+                </Card>
+
+                <Card variant = "outlined" sx = {{p: 1, m: 1}}>
+
+                    <p>
+                        This site is currently under development as a proof of concept. <br></br>
+
+                        If you would like to contact the admin, please send an email to LeftistMediaGroup.com<br></br>
+
+                    </p>
+                </Card>
             </Card>
 
-            <Card variant = "outlined" sx = {{p: 1, m: 1}}>
-
-                <p>
-                    This site is currently under development as a proof of concept. <br></br>
-
-                    If you would like to contact the admin, please send an email to LeftistMediaGroup.com<br></br>
-
-                </p>
-            </Card>
-            
             </>
         );
     };
