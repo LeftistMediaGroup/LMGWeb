dc76593838e7e21e58a2a80032876b8715f773d8
DanielATucker <leftistmediagroup@gmail.com>
2022-12-09 21:37:30 -0500

    T

--- lmgweb/src/components/projects/projects.js
+++ lmgweb/src/components/projects/projects.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Card from '@mui/material/Card';
+import { Box } from "@mui/system";
+
 export default class Projects extends React.Component {
     constructor(props) {
         super(props)
@@ -15,54 +17,56 @@ export default class Projects extends React.Component {
             <>
 
             <Card variant = "outlined" sx = {{p: 1, m: 1}}>
-
+                <Box>
                 
-                <Card sx = {{p: 1, m: 1}}>
-                    <h1> Projects </h1>           
-                </Card>
-                
-
-                <Card variant="outlined" sx={{ flexGrow: 1, p: 1, m: 1 }}>
-                    <Card variant="outlined" sx={{p: 1, m: 1 }}>
-                        <h2> LMG - Memes</h2>
-
-                        <p> 
-                            LMG - Memes is a facebook page that shares leftist propaganda.
-                        </p>
+                    <Card sx = {{p: 1, m: 1}}>
+                        <h1> Projects </h1>           
                     </Card>
                     
 
                     <Card variant="outlined" sx={{ flexGrow: 1, p: 1, m: 1 }}>
-                        <h2> Wade </h2>
+                        <Card variant="outlined" sx={{p: 1, m: 1 }}>
+                            <h2> LMG - Memes</h2>
 
-                        <p>
-                            Wade is a community response system made to assist with the development, design, and organization <br/>
-                            of protests. Wade is the mixture of Software and personnel. <br/>                       
-                        </p>
+                            <p> 
+                                LMG - Memes is a facebook page that shares leftist propaganda.
+                            </p>
+                        </Card>
+                        
+
+                        <Card variant="outlined" sx={{ flexGrow: 1, p: 1, m: 1 }}>
+                            <h2> Wade </h2>
+
+                            <p>
+                                Wade is a community response system made to assist with the development, design, and organization <br/>
+                                of protests. Wade is the mixture of Software and personnel. <br/>                       
+                            </p>
+                        </Card>
+
+                        <Card variant="outlined" sx={{ flexGrow: 1, p: 1, m: 1 }}>
+                            <h2> Leftism Through Music </h2>
+
+                            <p>
+                                This course will provide listeners with a playlist of 80+ downloadable leftist songs to listen to and reflect <br/>
+                                on centered around leftist topics.<br/>
+                                Topics include: <br/>
+                                    -#Marxism <br/>
+                                    -#Anarchism <br/>
+                                    -#Communism <br/>
+                                    -#ACAB <br/>
+                                    -#Revolution <br/>
+                                    -#WorkersRights <br/>
+                                    -#Capitalism <br/>
+                                    -#BLM <br/>
+                                    -#RevolutionaryThought <br/>
+                                    -Much more! <br/>
+                                Students will be provided with a leftist playlist, lyrics, collaborative notes, mentors, and other multimedia aids. <br/>
+                            </p>
+                        </Card>
                     </Card>
-
-                    <Card variant="outlined" sx={{ flexGrow: 1, p: 1, m: 1 }}>
-                        <h2> Leftism Through Music </h2>
-
-                        <p>
-                            This course will provide listeners with a playlist of 80+ downloadable leftist songs to listen to and reflect <br/>
-                            on centered around leftist topics.<br/>
-                            Topics include: <br/>
-                                -#Marxism <br/>
-                                -#Anarchism <br/>
-                                -#Communism <br/>
-                                -#ACAB <br/>
-                                -#Revolution <br/>
-                                -#WorkersRights <br/>
-                                -#Capitalism <br/>
-                                -#BLM <br/>
-                                -#RevolutionaryThought <br/>
-                                -Much more! <br/>
-                            Students will be provided with a leftist playlist, lyrics, collaborative notes, mentors, and other multimedia aids. <br/>
-                        </p>
-                    </Card>
-                </Card>
+                </Box>
             </Card>
+
             </>
         );
     };
