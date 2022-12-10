3f81ad39b5d6bf8eb8f84142cd1e6193470e46bb
DanielATucker <leftistmediagroup@gmail.com>
2022-12-10 12:04:25 -0500

    UF

--- lmgweb/src/components/education/capitalism.js
+++ lmgweb/src/components/education/capitalism.js
@@ -24,21 +24,28 @@ export default class Capitalism extends React.Component {
                 
                 <Card sx = {{p: 1, m: 1}}>
                     <p>
-                        If you were to google Capitalism, you'd get a very different answer you're going to receive here. <br/>
-                        
+                        If you were to google Capitalism, you'd get a very different answer you're going to receive here. 
+                        <br/>
                         <br/>
                         
-                        The following statement is not to be taken lightheartedly. <br/>
-                        
+                        The following statement is not to be taken lightheartedly. 
+                        <br/>
                         <br/>
 
-                        If you, as an individual reading these words do not take the time to educate yourself on The effects that capitalism has had, you are personally turning your back on existence as a whole. <br/>
+                        If you, as an individual reading these words do not take the time to educate yourself on The effects that capitalism has had, you are personally turning your back on existence as a whole. 
+                        <br/>
+                        <br/>
 
-                        Capitalism faces as an economic system. An economic system that is branded best in the world. <br/>
+                        Capitalism faces as an economic system. An economic system that is branded best in the world. 
+                        <br/>
+                        <br/>
                         
                         Within Capitalism, you buy things in order to live. At first glance, this seems normal, "This is the way things have always been" one might think, but that idea is completely debunked. For example, Indigenous peoples, at least those of North America, did not use money to survive, instead they shared what they had as a community. <br/>
+                        <br/>
 
-                        The point here, is that Capitalism is an idea that someone came up with. That idea, that one must buy things in order to live has resulted in an unimaginably large amount of human, animal, and ecological suffering and death paralleled only by Mass Extinction events. <br/>
+                        The point here, is that Capitalism is an idea that someone came up with. That idea, that one must buy things in order to live has resulted in an unimaginably large amount of human, animal, and ecological suffering and death paralleled only by Mass Extinction events. 
+                        <br/>
+                        <br/>
                     </p>
                 </Card>
             </Paper>
