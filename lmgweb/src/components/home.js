acd0b754e06227fc2b043eb2a413c7ee550bc033
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2022-12-25 10:15:27 -0500

    UF

--- lmgweb/src/components/home.js
+++ lmgweb/src/components/home.js
@@ -32,14 +32,15 @@ export default class Home extends React.Component {
                             Leftist Media group is dedicated to Organizing the Left and starting the Revolution. 
                             <br />
                             <br />
-                            Accomplishing this task will require educating the masses on the effects of Capitalism.                             <br />
+                            Accomplishing this task will require educating the masses on the effects of Capitalism.                             
+                            <br />
                             The goal of overthrowing Capitalism will require many subtasks, as the only way to defeat it
                             is through the power of love, or in other words, a multifaceted attack from every angle possible. 
                             <br />
                             <br />
-                            There is only a finite amount of time left as Capitalism has sent us near the end of our <br />
-                            climate catastrophe and fascist leadership path. As such, Leftist Media Group is creating a number <br />
-                            of projects to be community lead, all focused on a specific avenue of dismantling Capitalism one brick <br />
+                            There is only a finite amount of time left as Capitalism has sent us near the end of our
+                            climate catastrophe and fascist leadership path. As such, Leftist Media Group is creating a number
+                            of projects to be community lead, all focused on a specific avenue of dismantling Capitalism one brick 
                             at a time.
                         </Card>
                     </Paper>
@@ -48,9 +49,13 @@ export default class Home extends React.Component {
            
                     <Card sx={{ flexGrow: 1, p: 1, m: 1 }}>
                         <p>
-                            This site is currently under development as a proof of concept. <br></br>
+                            This site is currently under development as a proof of concept.
+                            <br>
+                            </br>
 
-                            If you would like to contact the admin, please send an email to LeftistMediaGroup@Gmail.com<br></br>
+                            If you would like to contact the admin, please send an email to LeftistMediaGroup@Gmail.com
+                            <br>
+                            </br>
                         </p>
                     </Card>
                 </Paper>
