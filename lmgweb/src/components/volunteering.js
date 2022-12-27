5718ae49f469edb204c38f520b36e442fd87fb99
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2022-12-27 11:09:13 -0500

    UF

--- lmgweb/src/components/volunteering.js
+++ lmgweb/src/components/volunteering.js
@@ -45,14 +45,14 @@ export default class Volunteering extends React.Component {
                     </p>
                 </Card>
 
-                <Card>
+                <Card variant = "outlined" sx = {{p: 1, m: 1}}>
                     <h3> Positions </h3>
 
                     <p>
                     We are asking for 1 hour of your time a week for the cause.
                     </p>
 
-                    <Card>
+                    <Card variant = "outlined" sx = {{p: 1, m: 1}}>
                         <h4> Article writers </h4>
 
                         <p>
@@ -60,7 +60,7 @@ export default class Volunteering extends React.Component {
                         </p>
                     </Card>
 
-                    <Card>
+                    <Card variant = "outlined" sx = {{p: 1, m: 1}}>
                         <h4> Meme Distributors </h4>
 
                         <p>
@@ -68,7 +68,7 @@ export default class Volunteering extends React.Component {
                         </p>
                     </Card>
 
-                    <Card>
+                    <Card variant = "outlined" sx = {{p: 1, m: 1}}>
                         <h4> Software Developers </h4>
 
                         <p> 
@@ -76,7 +76,7 @@ export default class Volunteering extends React.Component {
                         </p>
                     </Card>
 
-                    <Card>
+                    <Card variant = "outlined" sx = {{p: 1, m: 1}}>
                         <h4> Project Managers </h4>
                         <p> 
                         Looking for people to help organize our team and keep things on track. Any skills in project management, or leadership appreciated.
@@ -84,7 +84,7 @@ export default class Volunteering extends React.Component {
                     </Card>
                 </Card>
 
-                <Card>
+                <Card variant = "outlined" sx = {{p: 1, m: 1}}>
                     <h3> Lets Go!</h3>
 
                     <p>
