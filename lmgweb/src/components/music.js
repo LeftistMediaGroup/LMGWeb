ecac0cf499c648cb0148f8c6c1b008f05ceaaae9
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2022-12-29 09:05:30 -0500

    Added jinke-music-player

--- lmgweb/src/components/music.js
+++ lmgweb/src/components/music.js
@@ -4,25 +4,23 @@ import Card from '@mui/material/Card';
 
 import Paper from '@mui/material/Paper';
 
-import ReactPlayer from 'react-player'
+import ReactJkMusicPlayer from 'react-jinke-music-player';
+import 'react-jinke-music-player/assets/index.css';
 
-import CoolPlayer from 'react-cool-music-player';
-import 'react-cool-music-player/dist/index.css';
 
 export default class Music extends React.Component {
     constructor(props) {
         super(props)
 
+        this.audioInstance = null
+
         this.state = {
-            "data": [
-                {
-                  src: 'https://wade.4a2ib8tj96gajw856jj7.xyz/vidChunk?id=2',
-                  artist: 'David Rovics',
-                  name: 'War on the Workers',
-                  id: '1',
-                }
-            ]
-        }
+            "audioLists": {
+                name: "Failed State",
+                musicSrc: "wade.4a2ib8tj96gajw856jj7.xyz/vidChunk?id=2",
+                singer: "David Rovics",
+            }
+        };
     };
 
     render() {
@@ -36,7 +34,40 @@ export default class Music extends React.Component {
                 </Card>
             </Paper>
 
-            <CoolPlayer data={this.state.data}/>
+            <ReactJkMusicPlayer
+            getAudioInstance={(instance) => {
+                this.audioInstance = instance
+            }} audioLists={this.state.audioLists} />
+
+            <button onClick={() => this.audioInstance.play()}>play</button>
+            <button onClick={() => this.audioInstance.pause()}>pause</button>
+            <button onClick={() => this.audioInstance.load()}>reload</button>
+            
+            <button onClick={() => this.audioInstance.currentTime = 40}>
+                change current play time
+            </button>
+            
+            <button onClick={() => this.audioInstance.playbackRate = 2}>
+                change play back rate
+            </button>
+            
+            <button onClick={() => this.audioInstance.volume = 0.2}>
+                change volume
+            </button>
+            
+            <button onClick={() => this.audioInstance.destroy()}>
+                destroy player
+            </button>
+            
+            <button onClick={this.audio.togglePlay}>toggle play</button>
+            <button onClick={this.audio.clear}>clear audio lists</button>
+            <button onClick={this.audio.playNext}>play next</button>
+            <button onClick={this.audio.playPrev}>play prev</button>
+            <button onClick={() => this.audio.playByIndex(1)}>play by index</button>
+            
+            <button onClick={() => this.audio.updatePlayIndex(1)}>
+                update play index
+            </button>
 
             </>
         );
