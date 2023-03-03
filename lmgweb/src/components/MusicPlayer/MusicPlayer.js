575cc69b820f68dfc5cb6bfa6f8ceb2eb7e1ec4e
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-03 07:47:52 -0500

    T

--- lmgweb/src/components/MusicPlayer/MusicPlayer.js
+++ lmgweb/src/components/MusicPlayer/MusicPlayer.js
@@ -1,6 +1,6 @@
 import React from "React";
 
-import styles from "../styles/MusicPlayer.module.css";
+import styles from "../../styles/MusicPlayer.module.css";
 
 
 export default function MusicPlayer() {
