f1ac335bbd12e9a880f8431d2f4787acc4eefa8b
Daniel Tucker <DanielATucker@Outlook.com>
2023-04-01 12:55:53 -0400

    T

--- leftist-media-group/src/app/basic/SignUpForm.js
+++ leftist-media-group/src/app/basic/SignUpForm.js
@@ -13,7 +13,9 @@ export class SignUpForm extends Component {
     };
   }
 
-  submit() {
+  submit(event) {
+    event.preventDefault();
+
     if (this.state.password === this.state.password2) {
       fetch(`https://Back.LeftistMediaGroup.org/register/submit`, {
         method: "POST",
@@ -50,7 +52,7 @@ export class SignUpForm extends Component {
                   Create an account to start Volunteering!{" "}
                 </p>
 
-                <form className="forms-sample" onSubmit="this.submit(); return false;">
+                <form className="forms-sample" onSubmit={this.submit(event)}>
                   <Form.Group>
                     <label htmlFor="InputEmail">Email</label>
                     <Form.Control
